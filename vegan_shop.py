ffcf92a4e596299ee453968cec01ec3ebb0557df
simone-santonoceto <simone.santonoceto@ste-industries.com>
2023-11-07 12:42:53 +0100

    first commit

--- vegan_shop.py
+++ vegan_shop.py
@@ -1,1 +1,19 @@
 ''' Vegan grocieries shop'''
+class Product:
+    def __init__(self, name, quantity, price) -> None:
+        self.name = name
+        self.quantity = quantity
+        self.price = price
+
+class Inventory:
+    def __init__(self) -> None:
+        self.products = []
+    
+    def update_product(self, product):
+        pass
+
+    def remove_product(self, product):
+        pass
+    
+    def add_product(self, product):
+        pass
